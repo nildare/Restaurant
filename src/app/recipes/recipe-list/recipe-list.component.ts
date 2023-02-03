b67edf4c20c794be808f8ec199db1aef1c0efdd8
nildare <52755147+nildare@users.noreply.github.com>
2023-02-03 11:48:25 +0530

    Add files via upload

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,Input,OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,7 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit{
- recipes: Recipe[]=[
+  recipes: Recipe[]=[
     new Recipe('A Test Recipe','This is simply a test','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHxCqFwfsfn5wnsc8r_bsbdKdJCUT0LEEu1Z6iKupTmUURL9N-6yqCfrxykRy--l7xiQc&usqp=CAU')
 
  ];
